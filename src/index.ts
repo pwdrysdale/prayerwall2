8a59a33850c43babd59f16438b2510562583c9e5
“[Peter <pwdrysdale@gmail.com>
2021-10-13 16:37:03 -0400

    basic server is up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,53 @@
-console.log("Getting things up and running");
+import express, { Express, Request, Response } from "express";
+import { ApolloServer } from "apollo-server-express";
+import { GraphQLSchema } from "graphql";
+import cors from "cors";
+import { buildSchema, Query, Resolver } from "type-graphql";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+
+const startUp = async () => {
+    try {
+        await createConnection();
+
+        const app: Express = express();
+
+        @Resolver()
+        class HelloResolver {
+            @Query()
+            hello(): string {
+                return "Hello world";
+            }
+        }
+
+        const schema: GraphQLSchema = await buildSchema({
+            resolvers: [HelloResolver],
+            // authChecker: authChecker
+        });
+
+        const server = new ApolloServer({
+            schema,
+            context: ({ req, res }): { req: Request; res: Response } => ({
+                req,
+                res,
+            }),
+        });
+
+        await server.start();
+
+        server.applyMiddleware({ app });
+
+        app.use(cors());
+
+        app.listen({ port: 4001 }, () => {
+            console.log("Server is listening on 4001");
+        });
+
+        return { app, server };
+    } catch (err) {
+        console.log(err);
+        console.log("Sorry, no server today");
+    }
+};
+
+startUp();
